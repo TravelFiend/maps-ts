6df15dbc2d1bcb7a855c2b15e281fff632135273
TravelFiend <mikegrace274@gmail.com>
2020-10-15 15:11:46 -0400

    User and Company classes in place

--- src/User.ts
+++ src/User.ts
@@ -1,9 +1,17 @@
-class User {
+import faker from 'faker';
+
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() { }
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: +faker.address.latitude(),  // the "+" here is the just about the same as "Number()"
+      lng: +faker.address.longitude()
+    }
+  }
 }
